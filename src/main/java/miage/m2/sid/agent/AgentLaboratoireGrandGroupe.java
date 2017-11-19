79ba59c324806ae4943f9f381ad626e0fc16d10b
Florian Muller <muller.florian57@gmail.com>
2017-11-19 23:32:30 +0100

    Add receiver behaviour + gson library

--- src/main/java/miage/m2/sid/agent/AgentLaboratoireGrandGroupe.java
+++ src/main/java/miage/m2/sid/agent/AgentLaboratoireGrandGroupe.java
@@ -1,16 +1,46 @@
 package miage.m2.sid.agent;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import miage.m2.sid.behaviour.BehaviourLaboratoireGrandGroupe;
 
 public class AgentLaboratoireGrandGroupe extends Agent{
+
+	private BehaviourLaboratoireGrandGroupe behaviour;
 
 	protected void setup(){
 		//initialisation de l'agent
 		System.out.println(this.getName()+" "+this.getAID()+" started");
+		this.behaviour = new BehaviourLaboratoireGrandGroupe(this);
+		registerService();
+		addBehaviour(behaviour);
+	}
+
+	private void registerService() {
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(this.getAID());
+
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Labo");
+		sd.setName("LaboGrandGroupe");
+
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException e) {
+			System.err.println(getLocalName() +
+					" registration with DF unsucceeded. Reason: " + e.getMessage());
+			doDelete();
+		}
+
 	}
 	
 	protected void takeDown(){
-		//traitement de fin 
+		//traitement de fin
+		removeBehaviour(behaviour);
 		System.out.println(this.getName()+" "+this.getAID()+" done");
 	}
 	
--- src/main/java/miage/m2/sid/behaviour/BehaviourLaboratoireGrandGroupe.java
+++ src/main/java/miage/m2/sid/behaviour/BehaviourLaboratoireGrandGroupe.java
@@ -1,20 +1,31 @@
 package miage.m2.sid.behaviour;
 
+import com.google.gson.Gson;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
-public class BehaviourLaboratoireGrandGroupe extends CyclicBehaviour{
+public class BehaviourLaboratoireGrandGroupe extends CyclicBehaviour {
+    private Gson gson;
 
+    public BehaviourLaboratoireGrandGroupe(Agent agent) {
+        super(agent);
+        this.gson = new Gson();
+    }
 
-	@Override
-	public void action() {
-		// TODO Auto-generated method stub
-		ACLMessage message = null;
-		while(message==null){
-			message = this.getAgent().receive();
-		}
-		//action
-	}
-
+    @Override
+    public void action() {
+        ACLMessage aclMessage = myAgent.receive();
+        if (aclMessage != null) {
+            try {
+                String message = aclMessage.getContent();
+                System.out.println(myAgent.getLocalName() + ": I receive message\n" +
+                        aclMessage + "\nwith content\n" + message);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            this.block();
+        }
+    }
 }
